2d5bc6e410de288d5d6308be3c236a73846bedea
jorgemanhas <113157015+jorgemanhas@users.noreply.github.com>
2025-03-30 10:27:43 -0300

    Add files via upload

---
+++ python-chess-main/main.py
@@ -0,0 +1,40 @@
+import pygame
+
+from data.classes.Board import Board
+
+import pygame
+
+pygame.init()
+
+WINDOW_SIZE = (1000, 1000)
+screen = pygame.display.set_mode(WINDOW_SIZE)
+
+board = Board(WINDOW_SIZE[0], WINDOW_SIZE[1])
+
+def draw(display):
+	display.fill('white')
+
+	board.draw(display)
+
+	pygame.display.update()
+
+
+running = True
+while running:
+	mx, my = pygame.mouse.get_pos()
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+
+		elif event.type == pygame.MOUSEBUTTONDOWN:
+			if event.button == 1:
+				board.handle_click(mx, my)
+
+	if board.is_in_checkmate('black'):
+		print('Vitória Das Brancas!')
+		running = False
+	elif board.is_in_checkmate('white'):
+		print('Vitória das Pretas!')
+		running = False
+
+	draw(screen)
